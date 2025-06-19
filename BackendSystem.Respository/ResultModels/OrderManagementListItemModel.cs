c53d7172d33623863471937844fcfb672f3c14ac
RyanChen0831 <119422287+RyanChen0831@users.noreply.github.com>
2025-06-19 23:27:53 +0800

    feat(ordermanagement-datalayer): add OrderManagementListItem and OrderManagementOrderDetail model

---
+++ BackendSystem.Respository/ResultModels/OrderManagementListItemModel.cs
@@ -0,0 +1,14 @@
+﻿namespace BackendSystem.Respository.ResultModels
+{
+    public class OrderManagementListItemModel
+    {
+        public string OrderId { get; set; }
+        public string OrderDate { get; set; }
+        public int TotalAmount { get; set; }
+        public string ShippingAddress { get; set; }
+        public string ShippingStatus { get; set; }
+        public string Payment { get; set; }
+        public string PaymentStatus { get; set; }
+        public List<OrderManagementOrderDetailModel> OrderDetail { get; set; }
+    }
+}
---
+++ BackendSystem.Respository/ResultModels/OrderManagementOrderDetailModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BackendSystem.Respository.ResultModels
+{
+    public class OrderManagementOrderDetailModel
+    {
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public int SubTotal { get; set; }
+    }
+}
