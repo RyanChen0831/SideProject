e3eb1f1122ce7e6ef559f26984f588c64c2714f2
RyanChen0831 <119422287+RyanChen0831@users.noreply.github.com>
2025-06-19 23:26:40 +0800

    feat(ordermanagement-datalayer): add SearchQueryMode and DeleteOrderCommandModel

---
+++ BackendSystem.Respository/CommandModels/DeleteOrderCommandModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BackendSystem.Respository.CommandModels
+{
+    public class DeleteOrderCommandModel
+    {
+        public string OrderId { get; set; }
+        public string DeletedBy { get; set; }
+    }
+}
---
+++ BackendSystem.Respository/CommandModels/SearchOrderQueryModel.cs
@@ -0,0 +1,15 @@
+﻿namespace BackendSystem.Respository.CommandModels
+{
+    public class SearchOrderQueryModel
+    {
+        public string? OrderId { get; set; }
+        public string? MemberId { get; set; }
+        public string? MemberName { get; set; }
+        public string? ShippingStatus { get; set; }
+        public string? ShippingAddress { get; set; }
+        public string? OrderStatus { get; set; }
+        public string? Payment { get; set; }
+        public bool? IsCancel { get; set; }
+        public bool? IsDeleted { get; set; }
+    }
+}
