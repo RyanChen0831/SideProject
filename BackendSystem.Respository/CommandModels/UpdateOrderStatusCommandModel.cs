8996d0d1299e6161eafc06d105204c4d8e7aab37
RyanChen0831 <119422287+RyanChen0831@users.noreply.github.com>
2025-06-21 20:54:00 +0800

    feat(order): add UpdateOrderCommandModel for order update API

---
+++ BackendSystem.Respository/CommandModels/UpdateOrderStatusCommandModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BackendSystem.Respository.CommandModels
+{
+    internal class UpdateOrderStatusCommandModel
+    {
+        public int OrderId { get; set; }
+        public string OrderStatus { get; set; }
+        public string ShippingCompany { get; set; }
+        public string TrackingNumber { get; set; }
+        public string AdminNote { get; set; }
+        public int UpdatedBy { get; set; }
+    }
+}
