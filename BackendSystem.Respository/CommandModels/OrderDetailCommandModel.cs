24e1aa9034908dbd3ce57dfadddc2ea604b696f5
RyanChen0831 <119422287+RyanChen0831@users.noreply.github.com>
2025-06-21 20:59:24 +0800

    refactor(order): rename OrderDetailConditionModel to OrderDetailCommandModel for consistency

---
+++ BackendSystem.Respository/CommandModels/OrderDetailCommandModel.cs
@@ -0,0 +1,12 @@
+﻿
+namespace BackendSystem.Respository.CommandModels
+{
+    public class OrderDetailCommandModel
+    {
+        public string? OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public int UnitPrice { get; set; }
+        public int SubTotal { get; set; }
+    }
+}
