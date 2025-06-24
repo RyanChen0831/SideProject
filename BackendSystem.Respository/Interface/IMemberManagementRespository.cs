6d0a81451e1f9740a77395e589b58d43428f616e
RyanChen0831 <119422287+RyanChen0831@users.noreply.github.com>
2025-06-24 21:51:21 +0800

    refactor(membermanagement-data-layer): change IMemberManagementRepository interface to support new model

--- BackendSystem.Respository/Interface/IMemberManagementRespository.cs
+++ BackendSystem.Respository/Interface/IMemberManagementRespository.cs
@@ -1,13 +1,14 @@
 ﻿using BackendSystem.Respository.CommandModels;
-using BackendSystem.Respository.ResultModel;
+using BackendSystem.Respository.ResultModels;
+using System.Data;
 
 namespace BackendSystem.Respository.Interface
 {
     public interface IMemberManagementRespository
     {
-        public Task<IEnumerable<MemberManagementResultModel>> GetAllMember();
-        public Task<int> DeleteMember(MemberManagementCommandModel member);
-        public Task<int> UpdateMember(MemberManagementCommandModel member);
-        public Task<int> UpdateMemberVerificationStatus(int memberId);
+        public Task<IEnumerable<MemberManagementResultModel>> GetAllMember(IDbConnection conn);
+        public Task<int> DeleteMember(IDbConnection conn,IDbTransaction tx,MemberManagementCommandModel member);
+        public Task<int> UpdateMember(IDbConnection conn, IDbTransaction tx, MemberManagementCommandModel member);
+        public Task<int> UpdateMemberVerificationStatus(IDbConnection conn, IDbTransaction tx, int memberId);
     }
 }
