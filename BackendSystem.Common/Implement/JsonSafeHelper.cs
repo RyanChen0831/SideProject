f610cabd179a2047a9238521fc4be38d47ad05be
RyanChen0831 <119422287+RyanChen0831@users.noreply.github.com>
2025-06-29 17:01:51 +0800

    feat(json-helper): add JsonHelper utility for safe JSON conversion

---
+++ BackendSystem.Common/Implement/JsonSafeHelper.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+
+namespace BackendSystem.Common
+{
+    public static class JsonSafe
+    {
+        public static bool TryDeserialize<T>(string json, out T? result,out string? errorMessage)
+        {
+            if (string.IsNullOrWhiteSpace(json))
+            {
+                result = default;
+                errorMessage = "Input JSON string is null or empty.";
+                return false;
+            }
+
+            try
+            {
+                errorMessage = null;
+                result = JsonConvert.DeserializeObject<T>(json);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+                result = default;
+                return false;
+            }
+
+        }
+        public static bool TryDeserialize<T>(string json, out T? result)
+        {
+            return TryDeserialize<T>(json, out result, out _);
+        }
+
+    }
+}
+
