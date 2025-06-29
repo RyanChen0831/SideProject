895dbec838240468546d36d28e5f3692b7ac8002
RyanChen0831 <119422287+RyanChen0831@users.noreply.github.com>
2025-06-29 19:38:27 +0800

    feat(repo): add abstract factory interface and implementation for connection handling

---
+++ BackendSystem.Respository/Factory/DbConnectionFactory.cs
@@ -0,0 +1,19 @@
+﻿using BackendSystem.Respository.Interface;
+using Microsoft.Data.SqlClient;
+using System.Data;
+namespace BackendSystem.Respository.Factory
+{
+    public class DbConnectionFactory:IDbConnectionFactory
+    {
+        private readonly string _connectionString;
+        public DbConnectionFactory(string connectionString) 
+        {
+            _connectionString = connectionString;
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
---
+++ BackendSystem.Respository/Interface/IDbConnectionFactory.cs
@@ -0,0 +1,9 @@
+﻿using System.Data;
+
+namespace BackendSystem.Respository.Interface
+{
+    public interface IDbConnectionFactory
+    {
+        public IDbConnection CreateConnection();
+    }
+}
