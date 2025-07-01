d3f73a1a6b3936f12dbb30d0a8113b2b167757cc
RyanChen0831 <119422287+RyanChen0831@users.noreply.github.com>
2025-07-01 22:52:30 +0800

    feat(jwt-helper): add static helper for generating and validating JWT tokens

---
+++ BackendSystem.Common/Interface/IJWTHelper.cs
@@ -0,0 +1,11 @@
+﻿using BackendSystem.Common.Dtos;
+
+namespace BackendSystem.Common.Interface
+{
+    public interface IJWTHelper
+    {
+        public string GenerateToken(User user);
+        public int? ValidateToken(string token);
+
+    }
+}
---
+++ BackendSystem.Common/Settings/TokenSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace BackendSystem.Common.Model
+{
+    public class TokenSettings
+    {
+        public string Secret { get; set; }
+        public string Issuer { get; set; }
+        public int ExpireDays { get; set; }
+    }
+}
