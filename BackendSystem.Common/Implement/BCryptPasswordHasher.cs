27b6746391edc783e65d06258e20b83dc28c7def
RyanChen0831 <119422287+RyanChen0831@users.noreply.github.com>
2025-07-10 23:11:59 +0800

    feat(password-hash): add PasswordHasher and interface

---
+++ BackendSystem.Common/Implement/BCryptPasswordHasher.cs
@@ -0,0 +1,19 @@
+﻿
+
+using BackendSystem.Common.Interface;
+
+namespace BackendSystem.Common.Implement
+{
+    public class BCryptPasswordHasher : IPasswordHasher
+    {
+        public string Hash(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public bool Verify(string password, string passwordHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, passwordHash);
+        }
+    }
+}
