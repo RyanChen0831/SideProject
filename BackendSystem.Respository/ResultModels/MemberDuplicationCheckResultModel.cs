d7cea9847d92f3183a520555c38d00390c32c68c
RyanChen0831 <119422287+RyanChen0831@users.noreply.github.com>
2025-07-10 23:17:00 +0800

    refactor(models): remove models used by old methods

--- BackendSystem.Respository/ResultModels/MemberDuplicationCheckResultModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BackendSystem.Respository.ResultModels
-{
-    public class MemberDuplicationCheckResultModel
-    {
-        public string Account { get; set; }
-        public string Phone { get; set; }
-        public string Mail { get; set; }
-    }
-}
--- BackendSystem.Service/Dtos/ProductCategoryViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BackendSystem.Service.Dtos
-{
-    public class ProductCategoryViewModel
-    {
-        public int CategoryId { get; set; }
-        public string Category { get; set; }
-    }
-}
